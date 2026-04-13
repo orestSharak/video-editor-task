52057ae745c8773299584c41cd4ff71dfdd026c2
orestsharak <sharak.orest@gmail.com>
2026-04-13 22:03:05 +0200

    refactor: Added descriptive comments and make some selector sorting in the store.

--- video-editor/src/store/index.js
+++ video-editor/src/store/index.js
@@ -10,19 +10,24 @@ export const useVideoEditorStore = create((set, get) => ({
 }));
 
 // SELECTORS
+
+// Timeline & Clips
 export const useClips = () => useVideoEditorStore((state) => state.clips);
 export const usePlayHeadPosition = () => useVideoEditorStore((state) => state.playHeadPosition);
 export const useSelectedClipId = () => useVideoEditorStore((state) => state.selectedClipId);
-
 export const useSetPlayHeadPosition = () =>
   useVideoEditorStore((state) => state.setPlayHeadPosition);
 export const useSetSelectedClipId = () => useVideoEditorStore((state) => state.setSelectedClipId);
 export const useAddClip = () => useVideoEditorStore((state) => state.addClip);
 export const useSplitClip = () => useVideoEditorStore((state) => state.splitClip);
+
+// History
 export const useUndo = () => useVideoEditorStore((state) => state.undo);
 export const useRedo = () => useVideoEditorStore((state) => state.redo);
 export const useCanUndo = () => useVideoEditorStore((state) => state.past?.length > 0);
 export const useCanRedo = () => useVideoEditorStore((state) => state.future?.length > 0);
+
+// Project API
 export const useStatus = () => useVideoEditorStore((state) => state.status);
 export const useError = () => useVideoEditorStore((state) => state.error);
 export const useFetchProjects = () => useVideoEditorStore((state) => state.fetchProjects);
